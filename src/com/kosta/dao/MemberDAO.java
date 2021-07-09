e8cf100bd5eb3674c3ffd1fc5ef563ea81b06373
CSH <tmdghks260@naver.com>
2021-07-09 15:16:34 +0900

    insert 작동 확인

---
+++ src/com/kosta/dao/MemberDAO.java
@@ -0,0 +1,70 @@
+package com.kosta.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MemberDAO {
+	//db 기능 메서드
+	private Connection getConnection(){
+		String className = "oracle.jdbc.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String user = "hr";
+		String pwd = "hr";
+		Connection conn = null;
+		try {
+			Class.forName(className);
+			conn=DriverManager.getConnection(url,user,pwd);
+
+
+		}catch(SQLException | ClassNotFoundException e ) {
+			System.out.println(e);
+		}
+		return conn;
+	}
+
+	//insert 기능 메서드
+	//update 기능 메서드
+	//delete 기능 메서드
+	//select 기능 메서드
+	//close 기능 메서드
+
+	public int insert(String i, String p, String n,String em) {
+		Connection conn=getConnection();
+		PreparedStatement pstmt=null;
+
+		StringBuilder sql=new StringBuilder();
+		sql.append(" insert into account(id,name,email,pwd,no) ");
+		sql.append("  values(?,?,?,?,proseq.nextval)   "  	);
+
+
+		int result=0;
+		try {
+			pstmt=conn.prepareStatement(sql.toString());
+			pstmt.setString(1, i);
+			pstmt.setString(2, n);
+			pstmt.setString(3, em);
+			pstmt.setString(4, p);
+			result = pstmt.executeUpdate();
+
+		}catch(SQLException e) {
+			System.out.println(e);
+		}finally {
+			close(pstmt,conn);
+		}
+		return result;
+	}
+	private void close(PreparedStatement pstmt, Connection conn)//pstmt conn close기능
+	{
+		if(pstmt!=null) try { pstmt.close();} catch(SQLException e) {}
+		if(conn!=null) try { pstmt.close();} catch(SQLException e) {}
+	}
+
+	public getAll()
+	{
+		//자료구조 list map set => ArraryList : db연결후에 ArraryList에 담아 리턴
+		Connection conn=getConnection();
+
+	}
+}
---
+++ src/com/kosta/dao/MemberTest.java
@@ -0,0 +1,40 @@
+package com.kosta.dao;
+
+import java.util.Scanner;
+
+public class MemberTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 1.db
+		 * 2.preparedStatem insert update, delete => executeUpdate
+		 * 				select => executeQuery
+		 * 
+		 * 3.executeUpdate => row기능 
+		 * 	executeQuery => ResultSet
+		 * 4.close
+		 * 
+		 */
+		MemberDAO dao=new MemberDAO();
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("ID를 입력해주세요.");
+		String id=sc.nextLine();
+		System.out.println("비밀번호를 입력해주세요.");
+		String pwd=sc.nextLine();
+		System.out.println("이름를 입력해주세요.");
+		String name=sc.nextLine();
+		System.out.println("이메일를 입력해주세요.");
+		String email=sc.nextLine();
+
+		int result = dao.insert(id,pwd,name,email);
+
+		if(result >=1)
+			System.out.println("추가완료");
+		else
+			System.out.println("추가실패");
+
+	}
+
+}
