c3a721274bf6f797f18c27b8a665f59378f6f64e
CSH <tmdghks260@naver.com>
2021-07-09 17:45:33 +0900

    getall 메서드 추가 search 메서드 추가 프로그램 종료시 account table , seq 초기화 추가

--- src/com/kosta/dao/MemberDAO.java
+++ src/com/kosta/dao/MemberDAO.java
@@ -6,12 +6,13 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class MemberDAO {
-	
+
 	public HashMap<String, MemberDTO> hm=new HashMap<>();
-	
-	
+
+	Scanner sc=new Scanner(System.in);
 	private Connection getConnection(){ //db 기능 메서드
 		String className = "oracle.jdbc.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:XE";
@@ -34,10 +35,10 @@ public class MemberDAO {
 	//delete 기능 메서드
 	//select 기능 메서드 
 	//close 기능 메서드 o
-	private MemberDTO check(String i){
+	public MemberDTO check(String i){
 		MemberDTO result=null;
 		if(hm.containsKey(i)) {
-		result=hm.get(i);
+			result=hm.get(i);
 		}
 		return result;
 	}
@@ -82,9 +83,97 @@ public class MemberDAO {
 				MemberDTO value=hm.get(id);
 				System.out.println(id+"\t"+value);
 			}
-			
+			System.out.println("계속 조회할까요?");
+			String yn=sc.nextLine();
+			if(yn.contentEquals("n"))
+				break;
 		}
-	
+
+
+	}
+
+	public void search() {
+		while(true) {
+			System.out.println("ID를 입력해주세요.");
+			String id=sc.nextLine();
+			MemberDTO ck=check(id);
+			if(ck!=null) {
+				System.out.println("ID\t 이름\t 이메일\t");
+				Iterator<String> ita=hm.keySet().iterator();
+				id=ita.next();
+				MemberDTO value=hm.get(id);
+				System.out.println(id+"\t"+value);
+
+				System.out.println("계속 조회할까요? y/n");
+				String yn=sc.nextLine();
+				if(yn.contentEquals("n"))
+					break;
+			}
+			else
+				System.out.println("조회할 ID가 없습니다.");
+		}
+	}
+
+	public int exit() {
+		Connection conn=getConnection();
+		
+		PreparedStatement pstmt2=null;
+		StringBuilder sql2=new StringBuilder();
+		StringBuilder sql3=new StringBuilder();
+		StringBuilder sql4=new StringBuilder();
+		
+		sql2.append("  delete account  ");
+		sql3.append("  drop sequence proseq  ");
+		sql4.append("  create sequence proseq  ");
+	    int result2=0;
+		try {
+			pstmt2=conn.prepareStatement(sql3.toString());
+			pstmt2=conn.prepareStatement(sql4.toString());
+			pstmt2=conn.prepareStatement(sql2.toString());
+			result2=pstmt2.executeUpdate();
+
+		}catch(SQLException e) {
+			System.out.println(e);
+		}finally {
+			close(pstmt2,conn);
+		}
+		return result2;
+	}
+
+	public void cseq() {
+		Connection conn=getConnection();
+		PreparedStatement pstmt=null;
+		StringBuilder sql4=new StringBuilder();
+		sql4.append("  create sequence proseq  ");
+		try {
+			pstmt=conn.prepareStatement(sql4.toString());
+			pstmt.executeUpdate();
+		}catch(SQLException e) {
+			System.out.println(e);
+		}finally {
+			close(pstmt,conn);
+		}
+	}
+
+	public void dseq() {
+		Connection conn=getConnection();
+		PreparedStatement pstmt=null;
+		StringBuilder sql3=new StringBuilder();
+		sql3.append("  drop sequence proseq  ");
+		
+		try {
+			pstmt=conn.prepareStatement(sql3.toString());
+			pstmt.executeUpdate();
+		}catch(SQLException e) {
+			System.out.println(e);
+		}finally {
+			close(pstmt,conn);
+		} 
+		
+	}
+
+	public void modi() {
+		// TODO Auto-generated method stub
 		
 	}
 }
--- src/com/kosta/dao/MemberMain.java
+++ src/com/kosta/dao/MemberMain.java
@@ -25,6 +25,8 @@ public class MemberMain {
 				while(true) {
 				System.out.println("ID를 입력해주세요.");
 				String id=sc.nextLine();
+				MemberDTO ck=dao.check(id);
+				if(ck==null) {
 				System.out.println("비밀번호를 입력해주세요.");
 				String pwd=sc.nextLine();
 				System.out.println("이름를 입력해주세요.");
@@ -37,29 +39,36 @@ public class MemberMain {
 					System.out.println("추가완료");
 				else
 					System.out.println("추가실패");  
+				}
+				else
+					System.out.println("이미 존재하는 ID입니다.");
 				
 				System.out.println("계속 입력 하시겠습니까? y/n");
 				String yn=sc.nextLine();
 				if(yn.contentEquals("n"))
-					break;
+					break; 
 				}
 				break; 
 			case "2":
-
+				dao.modi();
 				break;
 			case "3":
 
 				break;
 			case "4":
-
+				dao.search();
 				break;
 			case "5":
 				dao.getAll();
 				break;
 			default:
+				dao.dseq();
+				dao.cseq();
+				int ex=dao.exit();
+				if(ex >=1) {
+				System.out.println("종료합니다.");
 				System.exit(0);
-
-
+				}
 
 			}
 
